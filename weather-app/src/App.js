310a379610dec60d6717dd1f24f13e29026e34a7
Habon <habonhassan01@gmail.com>
2022-03-02 18:37:02 -0500

    Add background img and styling to search bar

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
+const api = {
+  key: "e6028f3402d03c87f8daded950fc66db",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 function App() {
   return (
-    <div class="App">App</div>
+    <div class="app">
+      <main>
+      <div className='search-box'>
+      <input type='text' className="search-bar" placeholder='Search...'/>
+
+
+      </div>
+      </main>
+    </div>
   )
 }
 
