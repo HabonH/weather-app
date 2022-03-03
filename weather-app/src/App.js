a254a0ead487abdbbcf132f75fcbed9335ab1c89
Habon <habonhassan01@gmail.com>
2022-03-02 19:48:29 -0500

    Switch from fetch to axios

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 const api = {
   key: "e6028f3402d03c87f8daded950fc66db",
   base: "https://api.openweathermap.org/data/2.5/",
 };
+
 function App() {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
 
   const search = (event) => {
     if (event.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then((res) => res.json())
-        .then((result) => {
-          setWeather(result);
+      axios.get(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((response) => {
+          setWeather(response);
           setQuery("");
-          console.log("Here-->", result);
+          console.log("Here-->", response.data);
         });
     }
   };
