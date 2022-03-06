576e9494200a7abf41460726015eb021d20ec57b
Habon <habonhassan01@gmail.com>
2022-03-06 17:58:17 -0500

    Moved data to separate component

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Data from "./Data"
 
 const api = {
   key: "e6028f3402d03c87f8daded950fc66db",
   base: "https://api.openweathermap.org/data/2.5/",
 };
 
-function App() {
+function App(props) {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
 
@@ -22,40 +23,8 @@ function App() {
     }
   };
 
-  const dateBuilder = (d) => {
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`;
-  };
-
- 
   return (
     <div
       className={
@@ -78,12 +47,12 @@ function App() {
           />
         </div>
         {typeof weather.main != "undefined" ? (
-          <div>
+          <div className="weather-temp-container">
             <div className="location-box">
               <div className="location">
                 {weather.name}, {weather.sys.country}
               </div>
-              <div className="date">{dateBuilder(new Date())}</div>
+                <Data/>
             </div>
             <div className="weather-box">
               <div className="temp">{Math.round(weather.main.temp)}°C</div>
