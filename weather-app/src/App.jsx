04d660fe5dfa1966551917b0fc2220202f00f762
Habon <habonhassan01@gmail.com>
2022-03-06 18:06:34 -0500

    Created weather component to refactor App component

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,70 +1,8 @@
-import React, { useState } from "react";
-import axios from "axios";
-import Data from "./Data"
+import React from "react";
+import Weather from "./Weather";
 
-const api = {
-  key: "e6028f3402d03c87f8daded950fc66db",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
-
-function App(props) {
-  const [query, setQuery] = useState("");
-  const [weather, setWeather] = useState({});
-
-  const onSearch = (event) => {
-    if (event.key === "Enter") {
-      axios
-        .get(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then((response) => {
-          setWeather(response.data);
-          setQuery("");
-          console.log("Here-->", response.data.weather);
-        });
-    }
-  };
-
-
-
-  return (
-    <div
-      className={
-        typeof weather.main != "undefined"
-          ? weather.main.temp > 16
-            ? "app warm"
-            : "app"
-          : "app"
-      }
-    >
-      <main>
-        <div className="search-box">
-          <input
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
-            onChange={(event) => setQuery(event.target.value)}
-            value={query}
-            onKeyPress={onSearch}
-          />
-        </div>
-        {typeof weather.main != "undefined" ? (
-          <div className="weather-temp-container">
-            <div className="location-box">
-              <div className="location">
-                {weather.name}, {weather.sys.country}
-              </div>
-                <Data/>
-            </div>
-            <div className="weather-box">
-              <div className="temp">{Math.round(weather.main.temp)}°C</div>
-              <div className="weather">{weather.weather[0].main}</div>
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
-      </main>
-    </div>
-  );
+function App() {
+  return <Weather />;
 }
 
 export default App;
---
+++ weather-app/src/Weather.jsx
@@ -0,0 +1,65 @@
+import React, {useState} from 'react'
+import axios from "axios";
+import Data from "./Data"
+
+const api = {
+    key: "e6028f3402d03c87f8daded950fc66db",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+function Weather() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const onSearch = (event) => {
+    if (event.key === "Enter") {
+      axios
+        .get(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((response) => {
+          setWeather(response.data);
+          setQuery("");
+        });
+    }
+  };
+  return (
+    <div
+    className={
+      typeof weather.main != "undefined"
+        ? weather.main.temp > 16
+          ? "app warm"
+          : "app"
+        : "app"
+    }
+  >
+    <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(event) => setQuery(event.target.value)}
+            value={query}
+            onKeyPress={onSearch}
+          />
+        </div>
+        {typeof weather.main != "undefined" ? (
+          <div className="weather-temp-container">
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+                <Data />
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°C</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
+    </div>
+  )
+}
+
+export default Weather
